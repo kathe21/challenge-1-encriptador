c6235d46be4a7f6ddd8e3b790477f8f9a2394f9f
katthe21 <katthe21@hotmail.com>
2022-06-23 17:07:38 -0600

    primer repo

---
+++ encrip.js
@@ -0,0 +1,46 @@
+/*e--> enter 
+o--> ober
+i --> imes
+a --> ai
+u--> ufat
+*/
+
+function encriptar(){
+    var texto= document.getElementById("inputTexto").value.toLowerCase();
+    var txtCifrado = texto.replace(/e/igm,"enter");
+    var txtCifrado = txtCifrado.replace(/o/igm,"ober");
+    var txtCifrado = txtCifrado.replace(/i/igm,"imes");
+    var txtCifrado = txtCifrado.replace(/a/igm,"ai");
+    var txtCifrado = txtCifrado.replace(/u/igm,"ufat");
+
+    document.getElementById("muneco").style.display = "none";
+    document.getElementById("texto1").style.display = "none";
+    document.getElementById("cajatexto2").innerHTML= txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+
+
+}
+
+function desencriptar(){
+    var texto= document.getElementById("inputTexto").value.toLocaleLowerCase();
+    var txtCifrado= texto.replace(/enter/igm,"e");
+    var txtCifrado= txtCifrado.replace(/ober/igm,"o");
+    var txtCifrado= txtCifrado.replace(/imes/igm,"i");
+    var txtCifrado= txtCifrado.replace(/ai/igm,"a");
+    var txtCifrado= txtCifrado.replace(/ufat/igm,"u");
+
+    document.getElementById("muneco").style.display = "none";
+    document.getElementById("texto1").style.display = "none";
+    document.getElementById("cajatexto2").innerHTML= txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+
+}
+
+function copiar(){
+    var contenido = document.querySelector("#cajatexto2")
+    contenido.select();
+    document.execCommand("copy");
+    alert("se copio!");
+}
